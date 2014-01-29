06028f24528c7ca954829b267e935da9a1a31953
tcrawleyman <trevorcrawley96@gmail.com>
2014-01-29 13:59:07 -0600

    Starting Stuffs

---
+++ Platform.java
@@ -0,0 +1,28 @@
+import java.awt.Rectangle;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.geom.Line2D.Float;
+
+class Platform
+{
+	int tlX;
+	int tlY;
+	Rectangle rect;
+
+	public Platform(int tlX, int tlY)
+	{
+		this.tlX = tlX;
+		this.tlY = tlY;
+		this.rect = new Rectangle(0,0,1000,900);
+	}
+
+	public void drawPlatform(Graphics g, int height)
+	{
+		g.setColor(Color.orange);
+		g.fillArc(tlX,tlY+75+height,6,6,0,360);
+		g.fillRect(tlX+3,tlY+75+height,100,6);
+		g.fillArc(tlX+100,tlY+75+height,6,6,0,360);
+//		g.fillRect(tlX-48,700-tlY-48+height,154,1);
+		this.rect = new Rectangle(tlX-48,700-tlY-48-height,154,1);
+	}
+}
